24516d9d156e51b64cb5c074c0349533eab4cc66
vricheb973 <vricheb973@g.educaand.es>
2026-02-16 10:21:04 +0100

    generamos controlador

---
+++ src/main/java/ies/alcores/api_productos/controller/ProductoController.java
@@ -0,0 +1,24 @@
+package ies.alcores.api_productos.controller;
+
+import ies.alcores.api_productos.model.Producto;
+import ies.alcores.api_productos.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Producto>> findAll(){
+        return ResponseEntity.ok(this.productoService.findAll());
+    }
+}
