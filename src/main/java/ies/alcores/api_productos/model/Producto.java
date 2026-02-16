f6861905f349badae8727b9f78390177753d839c
vricheb973 <vricheb973@g.educaand.es>
2026-02-16 10:21:04 +0100

    generamos controlador

--- src/main/java/ies/alcores/api_productos/model/Producto.java
+++ src/main/java/ies/alcores/api_productos/model/Producto.java
@@ -2,9 +2,11 @@ package ies.alcores.api_productos.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.util.Date;
 
+@Data
 @Entity
 public class Producto {
 
---
+++ src/main/java/ies/alcores/api_productos/service/ProductoService.java
@@ -0,0 +1,20 @@
+package ies.alcores.api_productos.service;
+
+import ies.alcores.api_productos.model.Producto;
+import ies.alcores.api_productos.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> findAll(){
+        return this.productoRepository.findAll();
+    }
+
+}
